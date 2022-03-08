f81821bd9e8a2a000ba5950982f0bd013347724b
kalken12 <kalyaniknkr@gmail.com>
2022-03-08 21:19:33 +0530

    changes for sort

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ let i = 0;
 let id;
 id = setInterval(function () {
 
-    console.log(images.length)
+    //  console.log(images.length)
     if (i === images.length) {
         i = 0;
     }
@@ -27,7 +27,7 @@ id = setInterval(function () {
 
 var movieData = [
     {
-        id: 1,
+        id: 0,
         name: "No Time To Die",
 
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/079cc67ccc39c4f9407650cd74eded843d298363edab10fc7e1a1c1dd39ffab2._UR1920,1080_RI_UX400_UY225_.jpg",
@@ -36,7 +36,7 @@ var movieData = [
         ret: "5"
     },
     {
-        id: 2,
+        id: 1,
         name: "Sardar Udham",
 
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/c1e802d67e655b946c62d378adc9c8506e485e91600a4c239cf184c2ef921a6c._UR1920,1080_RI_UX400_UY225_.jpg",
@@ -46,7 +46,7 @@ var movieData = [
 
     },
     {
-        id: 3,
+        id: 2,
         name: "Shershaah",
 
         url: "https://images-eu.ssl-images-amazon.com/images/S/atv-aps-images/encoded/DHARMA_SHAH/IN/en_US.hi_IN/COVER_ART/NEW_MOVIE/Romance._UR1920,1080_RI_UX400_UY225_.jpg",
@@ -56,17 +56,17 @@ var movieData = [
 
     },
     {
-        id: 5,
+        id: 3,
         name: "Dybbuk",
 
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/78aedb083bf31cd21d442b2598bcfecc6847d44d8c42b15e8890bd35299b1ba1._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "Mahi, a newly married woman, brings an antique Jewish box into her home. When Mahi and her husband Sam begin to have",
         time: "5.11 h 52 min2021",
-        ret: "13+"
+        ret: "1"
 
     },
     {
-        id: 6,
+        id: 4,
         name: "Jai Bhim (Hindi)",
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/b65ffce505f1589f9e23e7147cb548e303a4f3adc8b9d65e3fa17805b1ca2350._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody.",
@@ -75,27 +75,27 @@ var movieData = [
 
     },
     {
-        id: 7,
+        id: 5,
         name: "Gold",
 
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3a06db2d9eb9bb302eddd1cad1d94df9765b1bb138cb13b6055eeb0505b30eba._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "Set in 1948, the historical story of India's first Olympic medal posts their independence.",
         time: "7.42 h 25 min2018",
-        ret: "4"
+        ret: "2"
 
     },
     {
-        id: 8,
+        id: 6,
         name: "premam",
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/a929aaa1b264df0121343ba56a62e691e68ee6c1ffc048f5763961ad57b11b59._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "Vijay Krishna is a guy who is called a loser because of bad luck and due to lack of social skills. He is often misunderstood and rejected",
         time: "7.42 h 25 min2018",
-        ret: "4",
+        ret: "2  ",
 
     },
 
     {
-        id: 9,
+        id: 7,
         name: "Kesari",
 
         url: "https://images-eu.ssl-images-amazon.com/images/S/atv-aps-images/encoded/KESARI/en_US/COVER_ART/STILL5._UR1920,1080_RI_UX400_UY225_.jpg",
@@ -106,7 +106,7 @@ var movieData = [
     },
 
     {
-        id: 10,
+        id: 8,
         name: "Majili",
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/3a06db2d9eb9bb302eddd1cad1d94df9765b1bb138cb13b6055eeb0505b30eba._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "Poorna (Naga Chaitanya) is a youth from Vizag who falls in love with Anju (Divyansha Kaushik), a rich naval officer's daughter. As",
@@ -115,7 +115,7 @@ var movieData = [
 
     },
     {
-        id: 11,
+        id: 9,
         name: "Dum Laga Ke Haisha",
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/9358d9ae6f31cca43879ee88ee53de4783165556916c49eb5e202940400c7f6b._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "Prem is a shy music buff who runs a tape recording shop. Sandhya is about to carve a niche for herself as a teacher. Will a regular",
@@ -133,7 +133,7 @@ var movieData = [
 
     },
     {
-        id: 10,
+        id: 11,
         name: "Chak De India",
         url: "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/f387592b927d1b21ace38e9f42f2525d4ec9f1b66029cd32ed8e96f873263b33._UR1920,1080_RI_UX400_UY225_.jpg",
         info: "Poorna (Naga Chaitanya) is a youth from Vizag who falls in love with Anju (Divyansha Kaushik), a rich naval officer's daughter. As",
@@ -141,11 +141,11 @@ var movieData = [
         ret: "4"
 
     }
-   
+
 
 ]
 var movies = document.querySelector("#movies");
-var moviesArr =[];
+var moviesArr = [];
 movieData.map(function (ele, index, arr) {
     var box = document.createElement("div");
     var img = document.createElement("img");
@@ -158,7 +158,7 @@ movieData.map(function (ele, index, arr) {
     p.textContent = ele.info;
     var flexDiv = document.createElement("div")
     //console.log(flexDiv);
-    flexDiv.setAttribute("id","flexDiv")
+    flexDiv.setAttribute("id", "flexDiv")
 
     var ret = document.createElement("h4")
     ret.textContent = ele.ret;
@@ -171,6 +171,52 @@ movieData.map(function (ele, index, arr) {
     box.append(img, p, name, flexDiv)
     movies.append(box);
     moviesArr.push(ele)
-    console.log(ele)
-    localStorage.setItem("movies",JSON.stringify(moviesArr))
+    //console.log(ele)
+    localStorage.setItem("movies", JSON.stringify(moviesArr))
 });
+
+
+
+////////////////////////sorting////// we are having two function ascending sort and descending sort
+var ArrRet = []
+var ArrId = [];
+var obj = {}
+movieData.map(function (ele, index, arr) {
+    ArrRet.push(ele.ret)
+    ArrId.push(ele.id)
+
+
+});
+
+//ArrRet.sort()
+console.log(ArrRet);
+//ArrId.sort()
+console.log(ArrId);
+for (let i = 0; i < ArrRet.length; i++) {
+    if (obj[ArrId[i]] == undefined) {
+        obj[ArrId[i]] = ArrRet[i]
+    }
+}
+console.log(obj)
+
+for(var key in obj)
+{
+    if(obj[key]==1)
+    {
+
+    }
+}
+
+
+///Ascedning sort
+//document.querySelector("#sort-lh").addEventListener("click", AscendingSort)
+
+
+
+
+
+
+
+//Descending sort
+
+
